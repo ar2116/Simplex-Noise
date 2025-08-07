52accd17ab08b2e9c397bfd9e4df3871e2f01dc6
A Sad Guy Coding <ar2116@student.riseinstitute.education>
2025-08-07 16:15:11 +0400

    Update simplex-noise.js

--- simplex-noise.js
+++ simplex-noise.js
@@ -1,57 +1,52 @@
-// Simplex Noise implementation
+// A robust SimplexNoise implementation
 class SimplexNoise {
     constructor(seed) {
-        this.grad3 = [
-            [1,1,0],[-1,1,0],[1,-1,0],[-1,-1,0],
-            [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1],
-            [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]
-        ];
-        this.p = new Array(256);
-        this.perm = new Array(512);
-        this.permMod12 = new Array(512);
-        
         this.F2 = 0.5 * (Math.sqrt(3.0) - 1.0);
         this.G2 = (3.0 - Math.sqrt(3.0)) / 6.0;
+        
+        this.grad3 = [
+            [1,1,0], [-1,1,0], [1,-1,0], [-1,-1,0],
+            [1,0,1], [-1,0,1], [1,0,-1], [-1,0,-1],
+            [0,1,1], [0,-1,1], [0,1,-1], [0,-1,-1]
+        ];
 
-        this.setSeed(seed);
+        this.p = new Array(256);
+        this.perm = new Array(512);
+        
+        this.seed(seed || 0);
     }
 
-    setSeed(seed) {
-        const rand = this.createRandomGenerator(seed);
-        
+    seed(seed) {
+        if(seed > 0 && seed < 1) {
+            seed *= 65536;
+        }
+
+        seed = Math.floor(seed);
+        if(seed < 256) {
+            seed |= seed << 8;
+        }
+
         for(let i = 0; i < 256; i++) {
+            let v;
+            if (i & 1) {
+                v = this.p[i] ^ (seed & 255);
+            } else {
+                v = this.p[i] ^ ((seed >> 8) & 255);
+            }
+
+            this.perm[i] = this.perm[i + 256] = v;
             this.p[i] = i;
         }
-
-        for(let i = 0; i < 255; i++) {
-            const r = i + ~~(rand() * (256 - i));
-            const aux = this.p[i];
-            this.p[i] = this.p[r];
-            this.p[r] = aux;
-        }
-
-        for(let i = 0; i < 512; i++) {
-            this.perm[i] = this.p[i & 255];
-            this.permMod12[i] = this.perm[i] % 12;
-        }
-    }
-
-    createRandomGenerator(seed) {
-        let value = seed;
-        return () => {
-            value = (value * 16807) % 2147483647;
-            return (value - 1) / 2147483646;
-        };
     }
 
     noise2D(xin, yin) {
-        const n0, n1, n2;
-        
+        let n0, n1, n2;
+
         const s = (xin + yin) * this.F2;
-        const i = Math.floor(xin + s);
-        const j = Math.floor(yin + s);
+        let i = Math.floor(xin + s);
+        let j = Math.floor(yin + s);
+
         const t = (i + j) * this.G2;
-        
         const X0 = i - t;
         const Y0 = j - t;
         const x0 = xin - X0;
@@ -71,41 +66,41 @@ class SimplexNoise {
         const x2 = x0 - 1.0 + 2.0 * this.G2;
         const y2 = y0 - 1.0 + 2.0 * this.G2;
 
-        const ii = i & 255;
-        const jj = j & 255;
+        i &= 255;
+        j &= 255;
 
-        const gi0 = this.permMod12[ii + this.perm[jj]];
-        const gi1 = this.permMod12[ii + i1 + this.perm[jj + j1]];
-        const gi2 = this.permMod12[ii + 1 + this.perm[jj + 1]];
+        const g0 = this.grad3[this.perm[(i + this.perm[j]) & 255] % 12];
+        const g1 = this.grad3[this.perm[(i + i1 + this.perm[j + j1]) & 255] % 12];
+        const g2 = this.grad3[this.perm[(i + 1 + this.perm[j + 1]) & 255] % 12];
 
-        let t0 = 0.5 - x0 * x0 - y0 * y0;
+        let t0 = 0.5 - x0*x0 - y0*y0;
         if(t0 < 0) {
             n0 = 0.0;
         } else {
             t0 *= t0;
-            n0 = t0 * t0 * this.dot(this.grad3[gi0], x0, y0);
+            n0 = t0 * t0 * this.dot(g0, x0, y0);
         }
 
-        let t1 = 0.5 - x1 * x1 - y1 * y1;
+        let t1 = 0.5 - x1*x1 - y1*y1;
         if(t1 < 0) {
             n1 = 0.0;
         } else {
             t1 *= t1;
-            n1 = t1 * t1 * this.dot(this.grad3[gi1], x1, y1);
+            n1 = t1 * t1 * this.dot(g1, x1, y1);
         }
 
-        let t2 = 0.5 - x2 * x2 - y2 * y2;
+        let t2 = 0.5 - x2*x2 - y2*y2;
         if(t2 < 0) {
             n2 = 0.0;
         } else {
             t2 *= t2;
-            n2 = t2 * t2 * this.dot(this.grad3[gi2], x2, y2);
+            n2 = t2 * t2 * this.dot(g2, x2, y2);
         }
 
         return 70.0 * (n0 + n1 + n2);
     }
 
     dot(g, x, y) {
-        return g[0] * x + g[1] * y;
+        return g[0]*x + g[1]*y;
     }
 }
